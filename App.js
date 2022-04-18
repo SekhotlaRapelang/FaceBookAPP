56b30f187d27bab8a3a24fb26fbd5c6409d29ea7
Rapelang Sekhotla <sekhotlarapelang@gmail.com>
2022-04-18 19:22:11 +0200

    fbpage

--- App.js
+++ App.js
@@ -97,7 +97,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height: '100%',
+    width: '100%',
     backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
